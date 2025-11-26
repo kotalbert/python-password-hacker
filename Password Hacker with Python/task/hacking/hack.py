d4f05eb437753cd79e9006c46ce821a5efb241c5
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-11-26 21:32:32 +0100

    Add socket communication to send messages in hack.py

--- Password Hacker with Python/task/hacking/hack.py
+++ Password Hacker with Python/task/hacking/hack.py
@@ -1,5 +1,5 @@
 """Password Hacker with Python - Hack Module"""
-
+import socket
 from argparse import ArgumentParser, Namespace
 
 
@@ -12,9 +12,22 @@ def parse_arguments() -> Namespace:
     args = parser.parse_args()
     return args
 
+def send_message(host: str, port: int, message: str) -> str:
+    # TCP client
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.settimeout(5)  # optional
+        sock.connect((host, port))
+        sock.sendall(message.encode("utf-8"))
+        # Receive (may need loop for larger data)
+        data = sock.recv(4096)
+        return data.decode("utf-8", errors="replace")
+
+
 
 def main():
     args = parse_arguments()
+    response = send_message(args.ip, args.port, args.msg)
+    print(response)
 
 if __name__ == "__main__":
     main()
