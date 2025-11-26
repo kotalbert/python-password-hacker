293ad6334f58baa854e34ce72795b9c421c907f0
Paweł Daniluk <pawel.daniluk@kotalbert.net>
2025-11-26 21:25:01 +0100

    Implement command line argument parsing in hack.py

--- Password Hacker with Python/task/hacking/hack.py
+++ Password Hacker with Python/task/hacking/hack.py
@@ -1,1 +1,20 @@
-# write your code here
+"""Password Hacker with Python - Hack Module"""
+
+from argparse import ArgumentParser, Namespace
+
+
+def parse_arguments() -> Namespace:
+    """Parse command line arguments"""
+    parser = ArgumentParser()
+    parser.add_argument("ip", type=str, help="IP Address")
+    parser.add_argument("port", type=int, help="Port")
+    parser.add_argument("msg", type=str, help="Message to send")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_arguments()
+
+if __name__ == "__main__":
+    main()
